1d4e052f5916bb03a7fd502756e137b2c5c45cee
Joeri332 <74733577+Joeri332@users.noreply.github.com>
2023-12-13 12:09:54 +0100

    test

--- PredictiveMaintenance/obj/Debug/net6.0/PredictiveMaintenance.AssemblyInfo.cs
+++ PredictiveMaintenance/obj/Debug/net6.0/PredictiveMaintenance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PredictiveMaintenance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b36575898132b074d60119505dd7c0ca0965a93")]
 [assembly: System.Reflection.AssemblyProductAttribute("PredictiveMaintenance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PredictiveMaintenance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
