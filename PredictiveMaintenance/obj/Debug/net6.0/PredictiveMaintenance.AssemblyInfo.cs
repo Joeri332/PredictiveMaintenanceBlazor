73d0fbaf90f3fb7c2d3441b9724ca0235b34ac98
Joeri332 <74733577+Joeri332@users.noreply.github.com>
2023-12-15 17:51:29 +0100

    Untracked files as per new .gitignore

--- PredictiveMaintenance/obj/Debug/net6.0/PredictiveMaintenance.AssemblyInfo.cs
+++ PredictiveMaintenance/obj/Debug/net6.0/PredictiveMaintenance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PredictiveMaintenance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d4e052f5916bb03a7fd502756e137b2c5c45cee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PredictiveMaintenance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PredictiveMaintenance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
